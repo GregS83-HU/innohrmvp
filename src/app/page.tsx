f3be86e3bca7808a1c6f362f9cfdc3000f93cff6
GregS83-HU <saussezg@icloud.com>
2025-08-09 09:22:28 +0200

    DEV: Home page Review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@
 export default function HomePage() {
   return (
     <div style={{ maxWidth: '700px', margin: 'auto', padding: '2rem' }}>
-      <h1 className="text-2xl font-bold text-center mb-6">Inno HR - Home Page</h1>
-      <p className="text-1xl font-bold text-center mb-6">HR was never as easy as NOW</p>
+      <h1 className="text-6xl font-bold text-center mb-6">Inno HR - Home Page</h1>
+       <img
+        src="/InnoHRLogo.jpeg"
+        alt="InnoHR"
+        style={{ display: 'block', margin: '0 auto' }}
+        />
+      <p className="text-2xl font-bold text-center mb-6">HR was never as easy as NOW</p>
     </div>
   )
 }
