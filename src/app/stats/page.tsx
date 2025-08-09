e81c2b40fdf4cbc6276e839161aa4037364515f4
GregS83-HU <saussezg@icloud.com>
2025-08-09 07:33:29 +0200

    FIX: Eslint

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -15,6 +15,7 @@ type PositionToCandidatRow = {
   candidats: Candidat[] | null
 }
 
+
 export default async function StatsPage({
   searchParams,
 }: {
@@ -59,7 +60,7 @@ export default async function StatsPage({
       cv_file
     )
   `)
-  .eq('position_id', Number(positionId))
+  .eq('position_id', Number(positionId)) as {data : PositionToCandidatRow[] | null, error: any}
 //console.log({ data, error });
 //console.dir(data, { depth: null }) // ou
 console.log('This is the JSON answer',JSON.stringify(data, null, 2))
