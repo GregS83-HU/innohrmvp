063f5ddfef1ebbfc275e164c0c6cdf6935d48d94
GregS83-HU <saussezg@icloud.com>
2025-09-13 12:18:14 +0200

    DEV: multiple improvements

--- src/app/jobs/[slug]/openedpositions/page.tsx
+++ src/app/jobs/[slug]/openedpositions/page.tsx
@@ -1,6 +1,6 @@
 
 // src/app/jobs/[slug]/page.tsx
-import PositionsList from "../../../openedpositions/PositionList";
+import PositionsList from "../../../OLDopenedpositions/PositionList";
 import { Analytics } from "@vercel/analytics/next"
 
 type Position = {
